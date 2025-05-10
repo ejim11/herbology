0f371210e1d41d49b056295f5bb6dc115d838739
ejim11 <favourejim56@gmail.com>
2025-05-10 21:14:05 +0100

    ✅ added the herb item comp

---
+++ components/HerbItem.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import formatAmount from "@/utils/formatAmount";
+
+const HerbItem = ({
+  image,
+  name,
+  title,
+  price,
+  slug,
+}: {
+  image: StaticImageData;
+  name: string;
+  title: string;
+  price: number;
+  slug: string;
+}) => {
+  return (
+    <Link
+      href={`/${slug.toLowerCase()}`}
+      className="flex flex-col text-center font-roboto"
+    >
+      <div className="w-full h-[50rem] rounded-[0.8rem] overflow-hidden">
+        <Image
+          src={image}
+          alt={`${title}-${name} image`}
+          priority
+          width={500}
+          height={800}
+          className="w-full h-full"
+        />
+      </div>
+      <p className="text-secondary-2 font-medium leading-[100%] mt-[2.4rem] mb-[1.2rem]">
+        {title}
+      </p>
+      <p className="text-secondary-1 text-[1.2rem] leading-[100%]">{name}</p>
+      <p className="mt-[3.2rem] font-medium text-secondary-2">
+        ₦{formatAmount(String(price))}
+      </p>
+    </Link>
+  );
+};
+
+export default HerbItem;
