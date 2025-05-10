d840177199519069822863ce513114dac0b886d1
ejim11 <favourejim56@gmail.com>
2025-05-10 21:12:16 +0100

    ✅ added the first section comp

---
+++ components/FirstSection.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import appContext from "@/store/appContext";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+} from "react";
+const FirstSection: React.FC<{
+  children: React.ReactNode;
+  containerClassName: string;
+}> = ({ children, containerClassName }) => {
+  const ctx = useContext(appContext);
+
+  const containerRef = useRef(null);
+
+  const options = useMemo(() => {
+    return {
+      root: null,
+      threshold: 0,
+      rootMargin: "-150px",
+    };
+  }, []);
+
+  const callBackFn = useCallback((entries: any, _observer: any) => {
+    const [entry] = entries;
+
+    ctx.changeStickyHeader(!entry.isIntersecting);
+  }, []);
+
+  useEffect(() => {
+    const current = containerRef.current;
+    const observer = new IntersectionObserver(callBackFn, options);
+
+    if (current) observer.observe(current);
+
+    return () => {
+      if (current) {
+        observer.unobserve(current);
+      }
+    };
+  }, [callBackFn, containerRef, options]);
+
+  return (
+    <div className={containerClassName} ref={containerRef}>
+      {children}
+    </div>
+  );
+};
+
+export default FirstSection;
