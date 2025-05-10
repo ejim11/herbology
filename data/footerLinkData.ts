fc532498572acf60e663b3b9ab64165e9d833513
ejim11 <favourejim56@gmail.com>
2025-05-10 21:16:34 +0100

    🗃️ added the footerlink data

---
+++ data/footerLinkData.ts
@@ -0,0 +1,52 @@
+export type FooterLinkData = {
+  title: string;
+  texts?: string[];
+  links?: { text: string; link: string }[];
+};
+
+export const footerLinksData: FooterLinkData[] = [
+  {
+    title: "Information",
+    links: [
+      {
+        text: "Shipping policy",
+        link: "/",
+      },
+      {
+        text: "Returns & Refunds",
+        link: "/",
+      },
+      {
+        text: "Privacy Policy",
+        link: "/",
+      },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      {
+        text: "About us",
+        link: "/about-us",
+      },
+      {
+        text: "Contact us",
+        link: "/contact-us",
+      },
+    ],
+  },
+  {
+    title: "Contact",
+    texts: ["123 Okpara Avenue, Off Bende Road, Umuahia, Abia State."],
+    links: [
+      {
+        text: "08012345678",
+        link: "tel:08012345678",
+      },
+      {
+        text: "info@herbology.com",
+        link: "mailto:info@herbology.com",
+      },
+    ],
+  },
+];
