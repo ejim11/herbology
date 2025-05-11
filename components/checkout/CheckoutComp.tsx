698def9deb5d921b44bff6f31f5f27f3814c05d7
ejim11 <favourejim56@gmail.com>
2025-05-11 22:19:08 +0100

    ✅ added the checkout comp

---
+++ components/checkout/CheckoutComp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CheckoutForm from "./CheckoutForm";
+
+const CheckoutComp = () => {
+  return (
+    <section className="flex w-full py-[7rem] px-[8rem] ">
+      <CheckoutForm />
+      
+    </section>
+  );
+};
+
+export default CheckoutComp;
