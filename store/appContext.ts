1ef8250546dd8ca0ce9aa338c9f9bb3becbd83b1
ejim11 <favourejim56@gmail.com>
2025-05-11 13:39:09 +0100

    ✅ updated app context with cart functionality

--- store/appContext.ts
+++ store/appContext.ts
@@ -1,16 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import React from "react";
+import { CartItem } from "./appContextProvider";
+import { HerbItemType } from "@/types/herbItem";
 
 type AppContextType = {
   isHeaderSticky: boolean;
   changeStickyHeader: (val: boolean) => void;
+  cartItems: CartItem[];
+  addItemToCart: (item: HerbItemType) => void;
+  increaseOrDecreaseCartItemQty: (item: HerbItemType, sign: string) => void;
+  removeCartItem: (item: CartItem) => void;
+  emptyCart: () => void;
 };
 
 const appContext = React.createContext<AppContextType>({
   isHeaderSticky: false,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   changeStickyHeader: (val: boolean) => {},
+  cartItems: [],
+  addItemToCart: (item: HerbItemType) => {},
+  increaseOrDecreaseCartItemQty: (item: HerbItemType, sign: string) => {},
+  removeCartItem: (item: CartItem) => {},
+  emptyCart: () => {},
 });
 
 export default appContext;
--- store/appContextProvider.tsx
+++ store/appContextProvider.tsx
@@ -1,14 +1,97 @@
 "use client";
 import React, { useState } from "react";
 import appContext from "./appContext";
+import { HerbItemType } from "@/types/herbItem";
+
+export type CartItem = { item: HerbItemType; choosenQty: number };
 
 const AppContextProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [isSticky, setIsSticky] = useState(false);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
   const onChangeStickerHeader = (val: boolean) => {
     setIsSticky(val);
+  };
+
+  const addItemToTheCart = (item: HerbItemType) => {
+    const existingItemIndex = cartItems.findIndex(
+      (dhItem: CartItem) => item.name === dhItem.item.name
+    );
+
+    const existingItem = cartItems[existingItemIndex];
+
+    if (existingItem) {
+      setCartItems((prevState: CartItem[]) => {
+        return prevState.map((cartItem: CartItem, index: number) => {
+          if (index === existingItemIndex) {
+            return {
+              item: cartItem.item,
+              choosenQty: cartItem.choosenQty + 1,
+            };
+          } else {
+            return {
+              item: cartItem.item,
+              choosenQty: cartItem.choosenQty,
+            };
+          }
+        });
+      });
+      return;
+    }
+
+    setCartItems((prevState: CartItem[]) => [
+      ...prevState,
+      { item: item, choosenQty: 1 },
+    ]);
+  };
+
+  const increaseOrDecreaseCartItemQty = (item: HerbItemType, sign: string) => {
+    const existingItemIndex = cartItems.findIndex(
+      (dhItem: CartItem) => item.name === dhItem.item.name
+    );
+
+    const existingItem = cartItems[existingItemIndex];
+
+    if (existingItem) {
+      if (sign === "minus" && existingItem.choosenQty === 1) {
+        removeCartItem(existingItem);
+        return;
+      }
+
+      setCartItems((prevState: CartItem[]) => {
+        return prevState.map((cartItem: CartItem, index: number) => {
+          if (index === existingItemIndex) {
+            return {
+              item: cartItem.item,
+              choosenQty:
+                sign === "plus"
+                  ? cartItem.choosenQty + 1
+                  : cartItem.choosenQty - 1,
+            };
+          } else {
+            return {
+              item: cartItem.item,
+              choosenQty: cartItem.choosenQty,
+            };
+          }
+        });
+      });
+      return;
+    }
+  };
+
+  const removeCartItem = (item: CartItem) => {
+    setCartItems((prevState: CartItem[]) =>
+      prevState.filter(
+        (ctItem: CartItem) => ctItem.item.name !== item.item.name
+      )
+    );
+  };
+
+  const emptyCart = () => {
+    setCartItems([]);
   };
 
   return (
@@ -16,6 +99,11 @@ const AppContextProvider: React.FC<{ children: React.ReactNode }> = ({
       value={{
         isHeaderSticky: isSticky,
         changeStickyHeader: onChangeStickerHeader,
+        cartItems: cartItems,
+        addItemToCart: addItemToTheCart,
+        increaseOrDecreaseCartItemQty: increaseOrDecreaseCartItemQty,
+        removeCartItem: removeCartItem,
+        emptyCart: emptyCart,
       }}
     >
       {children}
