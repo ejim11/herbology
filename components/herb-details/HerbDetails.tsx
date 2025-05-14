edc27b2fe912d2ef35c59f7862bf6639f4751a1f
ejim11 <favourejim56@gmail.com>
2025-05-15 00:12:45 +0100

    ✅ changed to category items and added the herb in the details

--- components/herb-details/HerbDetails.tsx
+++ components/herb-details/HerbDetails.tsx
@@ -2,16 +2,16 @@ import { HerbItemType } from "@/types/herbItem";
 import React, { FC } from "react";
 import Hero from "./Hero";
 import DescriptionAndRecommendation from "./DescriptionAndRecommendation";
-import TreatmentsCollection from "../TreatmentsCollection";
 import WhyChooseUs from "../WhyChooseUs";
 import FAQs from "../FAQs";
+import CategoryItems from "../CategoryItems";
 
 const HerbDetailsComp: FC<{ herb: HerbItemType }> = ({ herb }) => {
   return (
     <main className="flex flex-col">
       <Hero herb={herb} />
       <DescriptionAndRecommendation herb={herb} />
-      <TreatmentsCollection />
+      <CategoryItems herb={herb} />
       <section className="px-[8rem] max-xl:px-[5rem] max-xl:py-[8rem] max-lg:px-[3rem] max-md:px-[2rem] max-lg:py-[3rem] py-[12rem] bg-[rgba(245,247,251,1)]">
         <WhyChooseUs />
       </section>
