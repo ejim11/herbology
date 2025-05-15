5a2997fb5e85079945e3cd07ef2cfdb1fb3c3241
ejim11 <favourejim56@gmail.com>
2025-05-15 16:29:29 +0100

    📱 changed padding horizontally for the contact page

--- components/contact-us/ContactUsComp.tsx
+++ components/contact-us/ContactUsComp.tsx
@@ -7,7 +7,7 @@ const ContactUsComp = () => {
   return (
     <div className="flex w-full flex-col">
       <Hero />
-      <section className="px-[8rem] max-xl:px-[5rem] max-xl:py-[8rem] max-lg:px-[3rem] max-lg:py-[6rem] py-[12rem] bg-[rgba(245,247,251,1)]">
+      <section className="px-[8rem] max-xl:px-[5rem] max-xl:py-[8rem] max-lg:px-[3rem] max-md:px-[2rem] max-lg:py-[6rem] py-[12rem] bg-[rgba(245,247,251,1)]">
         <WhyChooseUs />
       </section>
       <FAQs />
