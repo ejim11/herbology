396455832ab90ec96784e697a354921f518c1a95
ejim11 <favourejim56@gmail.com>
2025-05-15 16:29:49 +0100

    ✅ added toast

--- components/herb-details/Hero.tsx
+++ components/herb-details/Hero.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { HerbItemType } from "@/types/herbItem";
 import formatAmount from "@/utils/formatAmount";
 import appContext from "@/store/appContext";
+import { FaRegCircleCheck } from "react-icons/fa6";
+import { toastSuccess } from "@/utils/toastFuncs";
 
 const Hero = ({ herb }: { herb: HerbItemType }) => {
   const { addItemToCart } = useContext(appContext);
@@ -17,14 +19,14 @@ const Hero = ({ herb }: { herb: HerbItemType }) => {
 
   return (
     <FirstSection containerClassName="flex min-h-[calc(100vh-8rem)] max-sm:flex-col">
-      <div className="w-[60%]  max-xl:w-[55%] max-lg:w-[50%] max-md:w-[45%]  max-sm:w-full max-sm:h-[55rem] ">
+      <div className="w-[60%]  max-xl:w-[55%] max-lg:w-[50%] max-md:w-[45%]  max-sm:w-full h-screen max-sm:h-[55rem] ">
         <Image
           src={herb.image}
           alt={`${herb.name} image`}
           priority
           width={1000}
           height={1000}
-          className="w-full h-full max-xlg:object-cover"
+          className="w-full h-full object-cover"
         />
       </div>
       <div className="w-[40%] max-xl:w-[45%] max-lg:w-[50%] max-md:w-[55%] max-sm:w-full max-sm:mt-[3rem] flex flex-col justify-center px-[5rem] max-xl:px-[3rem] max-md:px-[2rem] ">
@@ -79,6 +81,10 @@ const Hero = ({ herb }: { herb: HerbItemType }) => {
             }
 
             addItemToCart(herb);
+            toastSuccess(
+              `Item Added to Cart`,
+              <FaRegCircleCheck className="w-[2.3rem] h-[2.3rem] text-white" />
+            );
           }}
           className="mt-[4rem] block w-full cursor-pointer bg-secondary-2 text-center py-[2rem] font-roboto text-[1.4rem] leading-[100%] tracking-[1%] rounded-[0.8rem] text-white"
         >
