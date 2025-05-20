0e7950424ab82644b0d3d0c55858949a6c288ae7
ejim11 <favourejim56@gmail.com>
2025-05-20 21:38:17 +0100

    🎨 restructured the comp

--- components/herb-details/DescriptionAndRecommendation.tsx
+++ components/herb-details/DescriptionAndRecommendation.tsx
@@ -21,72 +21,70 @@ const DescriptionAndRecommendation = ({ herb }: { herb: HerbItemType }) => {
   ].filter((item) => item.value);
 
   return (
-    <section className="bg-[rgba(249,251,251,1)] flex justify-center max-sm:px-[2rem]">
-      <div className="w-[70%] max-lg:w-[80%] max-xmd:w-[90%] max-sm:w-full  py-[9rem] max-sm:py-[4rem]">
-        {data.map(
-          (item: { title: string; value: any; list?: any }, index: number) => {
-            return (
-              <AccordionItem
-                key={item.title}
-                index={index}
-                expanded={expanded}
-                setExpanded={setExpanded}
-                title={item.title}
-                titleClassName="uppercase"
-              >
-                {item.title === "full description" && (
-                  <div className="pt-[2rem] flex flex-col">
-                    {item.value.map((text: string) => (
-                      <p
-                        key={text}
-                        className="mb-[1.5rem] last:mb-0 text-[rgba(123,132,135,1)] text-[1.4rem] font-roboto leading-[2.4rem]"
-                      >
-                        {text}
-                      </p>
-                    ))}
-                    {item.list ? (
-                      <div className="font-roboto text-[1.4rem]">
-                        {item.list.title && (
-                          <p className="text-primary-1 mb-[1rem]">
-                            {item.list.title}
-                          </p>
-                        )}
-                        <ul className="flex flex-col">
-                          {item.list.list.map((text: string) => (
-                            <li
-                              key={text}
-                              className="flex items-center mb-[1rem] last:mb-0"
-                            >
-                              <SiCheckio className="w-[2.4rem] h-[2.4rem] mr-[1rem] text-primary-1" />
+    <div className="w-full ">
+      {data.map(
+        (item: { title: string; value: any; list?: any }, index: number) => {
+          return (
+            <AccordionItem
+              key={item.title}
+              index={index}
+              expanded={expanded}
+              setExpanded={setExpanded}
+              title={item.title}
+              titleClassName="uppercase"
+            >
+              {item.title === "full description" && (
+                <div className="pt-[2rem] flex flex-col">
+                  {item.value.map((text: string) => (
+                    <p
+                      key={text}
+                      className="mb-[1.5rem] last:mb-0 text-[rgba(123,132,135,1)] text-[1.4rem] font-roboto leading-[2.4rem]"
+                    >
+                      {text}
+                    </p>
+                  ))}
+                  {item.list ? (
+                    <div className="font-roboto text-[1.4rem]">
+                      {item.list.title && (
+                        <p className="text-primary-1 mb-[1rem]">
+                          {item.list.title}
+                        </p>
+                      )}
+                      <ul className="flex flex-col">
+                        {item.list.list.map((text: string) => (
+                          <li
+                            key={text}
+                            className="flex items-center mb-[1rem] last:mb-0"
+                          >
+                            <SiCheckio className="w-[2.4rem] h-[2.4rem] mr-[1rem] text-primary-1" />
 
-                              <span className="text-[rgba(123,132,135,1)] flex-1">
-                                {text}
-                              </span>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    ) : null}
-                  </div>
-                )}
-                {item.title === "recommendation" && (
-                  <div className="pt-[2rem]">
-                    {item.value.map((text: string) => (
-                      <p
-                        key={text}
-                        className="mb-[1.5rem] last:mb-0 text-[rgba(123,132,135,1)] text-[1.4rem] font-roboto leading-[2.4rem]"
-                      >
-                        {text}
-                      </p>
-                    ))}
-                  </div>
-                )}
-              </AccordionItem>
-            );
-          }
-        )}
-      </div>
-    </section>
+                            <span className="text-[rgba(123,132,135,1)] flex-1">
+                              {text}
+                            </span>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ) : null}
+                </div>
+              )}
+              {item.title === "recommendation" && (
+                <div className="pt-[2rem]">
+                  {item.value.map((text: string) => (
+                    <p
+                      key={text}
+                      className="mb-[1.5rem] last:mb-0 text-[rgba(123,132,135,1)] text-[1.4rem] font-roboto leading-[2.4rem]"
+                    >
+                      {text}
+                    </p>
+                  ))}
+                </div>
+              )}
+            </AccordionItem>
+          );
+        }
+      )}
+    </div>
   );
 };
 
