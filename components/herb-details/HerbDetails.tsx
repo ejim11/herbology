e4af877ffc3f0bdea97f1eeba2cc1328d00d8d62
ejim11 <favourejim56@gmail.com>
2025-05-20 21:38:47 +0100

    🔥 removed the description and recommendation comp

--- components/herb-details/HerbDetails.tsx
+++ components/herb-details/HerbDetails.tsx
@@ -1,7 +1,6 @@
 import { HerbItemType } from "@/types/herbItem";
 import React, { FC } from "react";
 import Hero from "./Hero";
-import DescriptionAndRecommendation from "./DescriptionAndRecommendation";
 import WhyChooseUs from "../WhyChooseUs";
 import FAQs from "../FAQs";
 import CategoryItems from "../CategoryItems";
@@ -10,7 +9,6 @@ const HerbDetailsComp: FC<{ herb: HerbItemType }> = ({ herb }) => {
   return (
     <main className="flex flex-col">
       <Hero herb={herb} />
-      <DescriptionAndRecommendation herb={herb} />
       <CategoryItems herb={herb} />
       <section className="px-[8rem] max-xl:px-[5rem] max-xl:py-[8rem] max-lg:px-[3rem] max-md:px-[2rem] max-lg:py-[3rem] py-[12rem] bg-[rgba(245,247,251,1)]">
         <WhyChooseUs />
