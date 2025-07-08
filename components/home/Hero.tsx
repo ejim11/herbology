2cd0497f6f89091ae7e005e5086b1544b90e1cad
ejim11 <favourejim56@gmail.com>
2025-07-08 13:00:32 +0100

    ✅ updated the home hero text

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+// import Link from "next/link";
 import React from "react";
 import FirstSection from "../FirstSection";
 
@@ -11,18 +11,18 @@ const Hero = () => {
             Shop organic remedies for a healthier you.
           </p>
           <h1 className="font-cambon  text-[7.6rem] max-smd:text-[6rem] text-white italic font-medium leading-[8.1rem] -tracking-[0.2rem] mb-[3.7rem] ">
-            Feel Better, Naturally
+            Do Better, Naturally
           </h1>
           <p className="font-roboto text-[2rem] text-[rgba(255,255,255,0.7)] leading-[2.9rem] font-light tracking-[0.1rem] mb-[3.2rem]">
-            Explore our carefully curated collection of herbal products and
-            experience pure, organic wellness
+            Explore our carefully curated collection of intimate herbal products
+            and experience peace of mind
           </p>
-          <Link
+          {/* <Link
             href={""}
             className="bg-[rgba(255,127,80,1)] py-[1.8rem] px-[3.2rem] rounded-[1.6rem] mt-[2.4rem] w-max text-white font-roboto"
           >
             Explore Wellness
-          </Link>
+          </Link> */}
         </div>
       </div>
     </FirstSection>
